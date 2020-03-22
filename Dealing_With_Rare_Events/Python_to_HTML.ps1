f6b38d9b7867bdb2b650f333cb64f185ae3b6069
Michael Siebel <michaeltsiebel@gmail.com>
2020-03-22 15:47:24 -0400

    Added About Page

--- Dealing_With_Rare_Events/Python_to_HTML.ps1
+++ Dealing_With_Rare_Events/Python_to_HTML.ps1
@@ -1,13 +1,13 @@
-﻿cd "C:\Users\siebe\Documents\16_GitHub\siebelm.github.io\US_Baby_Names"
+﻿cd "C:\Users\siebe\Documents\16_GitHub\siebelm.github.io\Dealing_With_Rare_Events"
 
 pygmentize -L styles
 
 # Convert Jupyter file to html
-jupyter nbconvert --to html "U.S. Baby Names.ipynb" --CSSHTMLHeaderPreprocessor.style=stata-dark 
+jupyter nbconvert --to html "Dealing With Rare Events.ipynb" --CSSHTMLHeaderPreprocessor.style=stata-dark 
 
 
 # Original file
-$orignal = "U.S. Baby Names.html"
+$orignal = "Dealing With Rare Events.html"
 # Output file
 $index = "index.html"
 
